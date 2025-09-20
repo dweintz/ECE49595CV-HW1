93976663b4b302fa2b728a90b8b5f95b29facdc2
dweintz <dweintz@purdue.edu>
2025-09-19 21:01:19 -0400

    Finished code

--- mlp_new.py
+++ mlp_new.py
@@ -1,4 +1,5 @@
-import math
+import math     # import for exp in sigmoid
+import random   # import to initilize network weights to random values
 
 # class for variables in neural network
 class Variable:
@@ -101,6 +102,123 @@ def sigmoid(x):
 
     return out
 
+# class for MLP neuron
+class Neuron:
+    def __init__(self, n_inputs):
+        self.W = [Variable(random.uniform(-1, 1)) for _ in range(n_inputs)]
+        self.b = Variable(0.0) 
+
+    # compute activation for neuron
+    def __call__(self, x):
+        weighted_sum = self.b
+        for wi, xi in zip(self.W, x):
+            weighted_sum = weighted_sum + wi * xi
+        return sigmoid(weighted_sum)
+
+# class for MLP layer  
+class Layer:
+    def __init__(self, n_inputs, n_outputs):
+        self.neurons = []
+        for _ in range(n_outputs):
+            neuron = Neuron(n_inputs)
+            self.neurons.append(neuron)
+    
+    # compute activations for layer
+    def __call__(self, x):
+        outputs = []
+        for neuron in self.neurons:
+            outputs.append(neuron(x))
+        return outputs
+
+# class for multi-layer perceptron
+class MLP:
+    def __init__(self, n_inputs, hidden_sizes, n_outputs):
+        sizes = [n_inputs] + hidden_sizes + [n_outputs]
+        self.layers = []
+        for i in range(len(sizes) - 1):
+            layer = Layer(sizes[i], sizes[i + 1])
+            self.layers.append(layer)
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+# loss function
+def loss_function(y_pred, y_true):
+    differences = [(yp - yt) * (yp - yt) for yp, yt in zip(y_pred, y_true)]
+    return sum(differences, Variable(0.0))
+
+# xor dataset
+def xor_dataset():
+    xor = [
+        ([0.0, 0.0], [0.0]),
+        ([0.0, 1.0], [1.0]),
+        ([1.0, 0.0], [1.0]),
+        ([1.0, 1.0], [0.0]),
+    ]
+    return xor
+
+# function to get prediction on new input
+def predict(net, x_raw):
+    # convert value to Variable object
+    x_vars = [Variable(v) for v in x_raw]
+
+    y_pred_vars = net(x_vars)
+
+    return [v.value for v in y_pred_vars]
+
+def main():
+    # create MLP
+    net = MLP(n_inputs = 2, hidden_sizes = [3], n_outputs = 1)
+
+    dataset = xor_dataset()
+
+    # train MLP
+    for epoch in range(10000):
+        total_loss = 0.0
+
+        for x_raw, y_raw in dataset:
+            # convert dataset values into instances of Variable class
+            x_vars = [Variable(v) for v in x_raw]
+            y_vars = [Variable(v) for v in y_raw]
+
+            # foward pass
+            y_pred = net(x_vars)
+            loss = loss_function(y_pred, y_vars)
+
+            # reset gradients for weights and biases
+            for layer in net.layers:
+                for neuron in layer.neurons:
+                    for W in neuron.W:
+                        W.grad = 0
+                    neuron.b.grad = 0
+            
+            # backprop step
+            loss.backprop()
+
+            # update weights and biases
+            eta = 0.1
+            for layer in net.layers:
+                for neuron in layer.neurons:
+                    for W in neuron.W:
+                        W.value = W.value - (eta * W.grad)
+                    neuron.b.value = neuron.b.value - (eta * neuron.b.grad)
+
+            # compute loss
+            total_loss += loss.value
+        
+        # print training progress to terminal
+        print(f"Epoch: {epoch}, Loss = {total_loss:.4f}")
+    
+    # make predictions
+    pred = predict(net, [0, 0])
+    print(pred)
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
@@ -108,17 +226,14 @@ def sigmoid(x):
 
 
 
+# x = Variable(-2.0)
+# y = Variable(5.0)
+# z = Variable(-4.0)
 
+# q = x + y
+# f = q * z
 
+# f.backprop()
 
+# print(f"f = {f.value}, df/dx = {x.grad}, df/dy = {y.grad}, df/dq = {q.grad}, df/df = {f.grad}")
 
-x = Variable(-2.0)
-y = Variable(5.0)
-z = Variable(-4.0)
-
-q = x + y
-f = q * z
-
-f.backprop()
-
-print(f"f = {f.value}, df/dx = {x.grad}, df/dy = {y.grad}, df/dq = {q.grad}, df/df = {f.grad}")
